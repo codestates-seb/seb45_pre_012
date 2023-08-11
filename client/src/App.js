f45c573b05a7031b134f2c381251f878dfd8be09
min <69198709+matchaing@users.noreply.github.com>
2023-08-11 16:51:06 +0900

    페이지 이동 기능

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import SignUpPage from './pages/SignUpPage.jsx';
 import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/signup" element={<SignUpPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 export default App;
