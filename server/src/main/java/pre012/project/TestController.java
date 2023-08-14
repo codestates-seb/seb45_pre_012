639d068da4a866be52f9d42ddc59779db9ba4289
newbee9507 <cho6185@naver.com>
2023-08-14 22:05:00 +0900

    test commit

---
+++ server/src/main/java/pre012/project/TestController.java
@@ -0,0 +1,13 @@
+package pre012.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String hello() {
+        return "hello";
+    }
+}
