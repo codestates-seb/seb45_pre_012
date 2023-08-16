32d9ae66142325a8e48f423e6036c0ce594304ba
matchaing <alswn202020@gmail.com>
2023-08-16 17:05:23 +0900

    주석 제거

--- client/src/App.js
+++ client/src/App.js
@@ -9,24 +9,24 @@ import Question from './pages/Question.jsx';
 //부트스트랩 css가 적용이 안되는 경우 상위 js 파일에 다음과 같이 bootstrap 관련 파일 import
 
 import SignUpPage from './pages/SignUpPage.jsx';
-// import Footer from './components/Footer.jsx';
+import Footer from './components/Footer.jsx';
 
-// import SideMenu from './components/SideMenu';
-// import Header from './components/Header';
+import SideMenu from './components/SideMenu';
+import Header from './components/Header';
 
 function App() {
   return (
     <Router>
       <div className="App"></div>
-      {/* <Header></Header> */}
-      {/* <SideMenu></SideMenu> */}
+      <Header></Header>
+      <SideMenu></SideMenu>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/question" element={<Question />} />
         <Route path="/signup" element={<SignUpPage />} />
       </Routes>
-      {/* <Footer /> */}
+      <Footer />
     </Router>
   );
 }
