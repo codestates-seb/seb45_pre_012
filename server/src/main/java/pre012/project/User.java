323d6c5dc29e04636bbb22a73acf07b8350cec21
newbee9507 <cho6185@naver.com>
2023-08-17 17:53:52 +0900

    db test k.b

--- server/src/main/java/pre012/project/User.java
+++ server/src/main/java/pre012/project/User.java
@@ -8,7 +8,7 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
-@Table(name = "`users`")
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
