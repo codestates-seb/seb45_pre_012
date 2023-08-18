0f1a06fae0327e745ffda9c65a675b81d5bf9200
newbee9507 <cho6185@naver.com>
2023-08-18 10:15:10 +0900

    signup test a.d

--- server/src/main/java/pre012/project/WebConfig.java
+++ server/src/main/java/pre012/project/WebConfig.java
@@ -3,14 +3,20 @@ package pre012.project;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    private final String s3URL = "http://prepro012.s3-website.ap-northeast-2.amazonaws.com";
-
-
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*") // 허용할 출처 설정
+                .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 메서드 설정
+                .allowedHeaders("*"); // 허용할 헤더 설정
+    }
 }
