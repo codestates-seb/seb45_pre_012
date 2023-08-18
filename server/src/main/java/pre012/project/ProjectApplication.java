1c4c1fa11d1caf74ceb8a520d2675f27d0fa552b
newbee9507 <cho6185@naver.com>
2023-08-18 17:19:36 +0900

    table auto test a

--- server/src/main/java/pre012/project/ProjectApplication.java
+++ server/src/main/java/pre012/project/ProjectApplication.java
@@ -2,8 +2,10 @@ package pre012.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan({"pre012.project.answer.entity", "pre012.project.question.entity"})
 public class ProjectApplication {
 
 	public static void main(String[] args) {
