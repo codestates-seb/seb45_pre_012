45b78b64b1c71dc59c3ffa328030467fb264e856
newbee9507 <cho6185@naver.com>
2023-08-21 11:41:07 +0900

    @Component

--- server/src/main/java/pre012/project/answer/mapper/AnswerMapper.java
+++ server/src/main/java/pre012/project/answer/mapper/AnswerMapper.java
@@ -2,6 +2,7 @@ package pre012.project.answer.mapper;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.stereotype.Component;
 import pre012.project.answer.dto.AnswerPatchDTO;
 import pre012.project.answer.dto.AnswerPostDTO;
 import pre012.project.answer.dto.AnswerResponseDTO;
@@ -10,6 +11,7 @@ import pre012.project.answer.entity.Answer;
 import java.util.List;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface AnswerMapper {
     Answer answerPostDTOtoAnswer(AnswerPostDTO answerPostDTO);
     Answer answerPatchDTOtoAnswer(AnswerPatchDTO answerPatchDTO);
