41cd619a3f6ce7d133ca843f7d0fcbf0d64e5451
LEEHYUNGSU <gudtn0506@gmail.com>
2023-08-22 12:50:15 +0900

    LoginReducer

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -4,46 +4,46 @@ import LoginForm from '../components/LoginForm.jsx';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SocialLogin from '../components/SocialLogin.jsx';
 import BaseHeader from '../components/BaseHeader.jsx';
-import { useState } from 'react';
+import { useSelector } from 'react-redux';
 
 const Login = () => {
-  const [loginState, setLoginState] = useState(false);
+  const user = useSelector((state) => state.login.user);
 
   return (
-    <div className="vh-100 overflow-hidden">
-      <BaseHeader />
-      <Container
-        fluid
-        className="vh-100 w-100 d-flex flex-column bg-light mt-5"
-      >
-        <Row fluid className="vh-100 d-flex justify-content-center w-100 mt-5">
-          <Col lg={12}>
-            <Row fluid className="d-flex flex-column align-items-center">
-              <Col lg={6} className="mt-5" />
-              <Col lg={6} className="mt-5">
-                <SocialLogin />
-                {!loginState ? (
-                  <LoginForm
-                    loginState={loginState}
-                    setLoginState={setLoginState}
-                  />
-                ) : null}
-
-                <p className="text-center mt-3">
-                  {`Don't have an account?`}{' '}
-                  <Link to="/TestPage" className="sign_up_text">
-                    Sign up
-                  </Link>
-                </p>
+    <>
+      {user !== null ? (
+        <div>이미 로그인하였습니다</div>
+      ) : (
+        <div className="vh-100 overflow-hidden">
+          <BaseHeader />
+          <Container
+            fluid
+            className="vh-100 w-100 d-flex flex-column con-bg-light bg-none mt-5"
+          >
+            <Row
+              fluid
+              className="vh-100 d-flex justify-content-center w-100 mt-5"
+            >
+              <Col lg={12}>
+                <Row fluid className="d-flex flex-column align-items-center">
+                  <Col lg={6} className="mt-5" />
+                  <Col lg={6} className="mt-5">
+                    <SocialLogin />
+                    <LoginForm />
+                    <p className="text-center mt-3">
+                      {`Don't have an account?`}{' '}
+                      <Link to="/singup" className="sign_up_text">
+                        Sign up
+                      </Link>
+                    </p>
+                  </Col>
+                </Row>
               </Col>
             </Row>
-          </Col>
-        </Row>
-      </Container>
-      {loginState ? (
-        <p className="w-100 mt-5 bg-black">로그인 완료하였습니다</p>
-      ) : null}
-    </div>
+          </Container>
+        </div>
+      )}
+    </>
   );
 };
 
